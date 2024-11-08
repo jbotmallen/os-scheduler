f3e277176b71b297701403a9fe4630cf57c94ab6
Chuyaks123 <66161070+shetbricks21@users.noreply.github.com>
2024-11-08 17:47:05 +0800

    adjusted gantt chart now starts at 0

--- components/shared/pre-gantt-chart.tsx
+++ components/shared/pre-gantt-chart.tsx
@@ -13,6 +13,18 @@ const PreemptiveGanttChart: React.FC<GanttChartProps> = ({ algorithm, schedule, 
         <div className="mt-8 w-full mx-auto">
             <h2 className="text-xl font-semibold -mb-5">Gantt Chart (Preemptive Scheduling)</h2>
             <div className="relative flex items-center gap-0">
+                {schedule[0].start > 0 && (
+                    <div
+                        className="border-black border-2 border-dashed mt-10 text-gray-500 flex items-center justify-center font-bold p-3 rounded-md shadow-md shadow-gray-400"
+                        style={{
+                            width: `${getPercentage(schedule[0].start, totalDuration)}%`,
+                            backgroundColor: 'lightgray',
+                        }}
+                    >
+                        Idle
+                        <span className="absolute text-sm -left-1.5 -bottom-6 text-black">00</span>
+                    </div>
+                )}
                 {schedule.map((slice, index) => (
                     <Fragment key={index}>
                         {index > 0 && slice.start > schedule[index - 1].end && (
@@ -46,6 +58,11 @@ const PreemptiveGanttChart: React.FC<GanttChartProps> = ({ algorithm, schedule, 
                             {algorithm === "Priority" && slice.priority !== undefined && (
                                 <span className="absolute text-xs top-1 right-1 text-gray-300">
                                     P{slice.priority}
+                                </span>
+                            )}
+                            {index + 1 === schedule.length && (
+                                <span className="absolute text-sm -right-1 -bottom-6 text-black">
+                                    {slice.end.toString().padStart(2, "0")}
                                 </span>
                             )}
                         </div>
